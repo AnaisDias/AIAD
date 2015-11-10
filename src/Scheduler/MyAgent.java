42749f30dfc3c8384a3433c1d3a460cdd3889bcd
Anaís Dias <dias4993@gmail.com>
2015-11-10 03:57:41 +0000

    DF register works?? Need to fix loop on subscription initiator

--- src/Scheduler/MyAgent.java
+++ src/Scheduler/MyAgent.java
@@ -1,21 +1,18 @@
 package Scheduler;
 
 import java.util.HashMap;
-import java.util.Iterator;
-
 import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.Property;
-import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.proto.SubscriptionInitiator;
 
 public class MyAgent extends Agent {
 
+	private static final long serialVersionUID = 1L;
 	public HashMap<String, AID> agents = new HashMap<>();
 
 	public MyAgent() {
@@ -30,27 +27,30 @@ public class MyAgent extends Agent {
 		// Register the service
 	  	System.out.println("Agent "+getLocalName()+" registering service "+serviceName);
 
-		DFAgentDescription dfd = new DFAgentDescription();
-        dfd.setName(getAID());
-        ServiceDescription sd = new ServiceDescription();
-        sd.setType("normal");
-        sd.setName("iScheduler");
-        dfd.addServices(sd);
-        try {
-			DFService.register(this, dfd);
-		} catch (FIPAException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	  	DFAgentDescription dfd = new DFAgentDescription();
+	      dfd.setName(getAID());
+	      ServiceDescription sd = new ServiceDescription();
+	      sd.setName(serviceName);
+	      sd.setType("Scheduler");
+	      dfd.addServices(sd);
+	      try {
+	         DFService.register(this, dfd);
+	      } catch(FIPAException e) {
+	         e.printStackTrace();
+	      }/*
 		// Build the description used as template for the subscription
 		DFAgentDescription template = new DFAgentDescription();
 		ServiceDescription templateSd = new ServiceDescription();
 		templateSd.setType("normal");
-		template.addServices(templateSd);
+		template.addServices(templateSd);*/
 
 		
-		addBehaviour(new SubscriptionInitiator(this,
-				DFService.createSubscriptionMessage(this, getDefaultDF(), template, null)) {
+		addBehaviour(new SubscriptionInitiator(this, DFService.createSubscriptionMessage(this, getDefaultDF(), dfd, null)) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			protected void handleInform(ACLMessage inform) {
 				System.out.println("Agent " + getLocalName() + ": Notification received from DF");
 				try {
@@ -59,14 +59,11 @@ public class MyAgent extends Agent {
 						for (int i = 0; i < results.length; ++i) {
 							DFAgentDescription dfd = results[i];
 							AID provider = dfd.getName();
-							// The same agent may provide several services; we
-							// are only interested
-							// in the normal one
 							//Iterator it = dfd.getAllServices();
 							while (dfd.getAllServices().hasNext()) {
 								ServiceDescription sd = (ServiceDescription) dfd.getAllServices().next();
-								if (sd.getType().equals("normal")) {
-									System.out.println("normal service for Italy found:");
+								if (sd.getType().equals("Scheduler")) {
+									System.out.println("Scheduler found:");
 									System.out.println("- Service \"" + sd.getName() + "\" provided by agent "
 											+ provider.getName());
 								//adicionar agente a lista
