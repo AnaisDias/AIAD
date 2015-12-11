7fa3e7b5d7e85107a537f4804d73503400f73705
Anaís Dias <dias4993@gmail.com>
2015-12-11 18:16:58 +0000

    added constraint code. still not done. need to substitute deprecated functions

--- src/Constraints/AfterHourConstraint.java
+++ src/Constraints/AfterHourConstraint.java
@@ -1,12 +1,26 @@
 package Constraints;
 
+import java.util.Date;
+
 import Utilities.TimePeriod;
 
 public class AfterHourConstraint implements Constraint{
 
+	public Date date;
+	
+	public AfterHourConstraint(Date date){
+		this.date=date;
+	}
+	
+	@SuppressWarnings("deprecation")//try to replace by non deprecated functions
 	@Override
 	public boolean satisfiedBy(TimePeriod tp) {
-		// TODO Auto-generated method stub
+		if(date.getHours()==tp.getStartTime().getHours()){
+			return (date.getMinutes()<tp.getStartTime().getMinutes());
+		}
+		else if(date.getHours() < tp.getStartTime().getHours()){
+			return true;
+		}
 		return false;
 	}
 
