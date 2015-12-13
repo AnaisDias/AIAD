c9f8904dacaf16dbdc1d6a6f6e959345571cc4bc
tantch <ei12019@fe.up.pt>
2015-12-13 23:31:41 +0000

    janela de soluçao

--- src/gui/solutionController.java
+++ src/gui/solutionController.java
@@ -2,6 +2,9 @@ package gui;
 
 import java.lang.reflect.Array;
 
+import Scheduler.MyEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -10,10 +13,19 @@ public class solutionController {
 	@FXML
 	ListView<String> sol;
 	@FXML
-	Label cost;
+	Label name;
 	//adicioanr aprametros qe or preciso pelo s
-	public solutionController(){
+	public solutionController(ObservableList<MyEvent> events, String string){
 		
+		name.setText(string);
+		ObservableList<String> temp = FXCollections.observableArrayList();
+		for (MyEvent event : events) {
+			
+			String temp2 = "Event - " + event.getName() + "  Solution : " + event.getAgreedTimePeriod().toString() + " Cost: " + event.getAgreedCost();
+			temp.add(temp2);
+			
+		}
+		sol.setItems(temp);
 	}
 	
 	@FXML
