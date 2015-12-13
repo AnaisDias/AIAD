310c4fdd4a7beefe943dd75faf9a4d018c96255c
vania-leite <ei12037@fe.up.pt>
2015-12-13 23:19:17 +0000

    edit

--- src/gui/solutionController.java
+++ src/gui/solutionController.java
@@ -1,11 +1,14 @@
 package gui;
 
+import java.lang.reflect.Array;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class solutionController {
 	@FXML
-	Label solution;
+	ListView<String> sol;
 	@FXML
 	Label cost;
 	//adicioanr aprametros qe or preciso pelo cosntrutor
